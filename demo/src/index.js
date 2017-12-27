97752019346aafd5da5b055f12da2535d6356082
ilya <ilya@methodexists.com>
2017-12-27 16:11:45 +0300

    Add RichEditor with custom image upload

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Card, Layout } from 'antd';
+import { message, Card, Layout } from 'antd';
 import RichEditor from '~';
+
+const imageUrl = 'https://loremflickr.com/320/240';
+
+const handleImageUpload = (file) => {
+  const hide = message.loading(`Uploading ${file.name}…`, 0);
+  return new Promise(resolve => setTimeout(() => {
+    hide();
+    resolve(imageUrl);
+  }, 500));
+};
 
 class Demo extends Component {
   render() {
@@ -13,6 +23,12 @@ class Demo extends Component {
         <Layout.Content style={{ padding: 32 }}>
           <Card style={{ padding: 30, marginBottom: 32 }}>
             <RichEditor />
+          </Card>
+          <Card
+            title="Custom image uploader"
+            style={{ padding: 30, marginBottom: 32 }}
+          >
+            <RichEditor onImageUpload={handleImageUpload} />
           </Card>
         </Layout.Content>
       </Layout>
--- src/index.js
+++ src/index.js
@@ -1,25 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import './styles.less';
 
-function RichEditor({ className, style }) {
-  // remember to use BEM for styles instead of CSS Modules
-  const classNames = `my-component ${className}`;
-  return (
-    <div className={classNames} style={style}>
-      RichEditor
-    </div>
-  );
+class RichEditor extends React.Component {
+  imageHandler = () => {
+    const editor = this.quillRef.getEditor();
+    const fileInput = document.createElement('input');
+    fileInput.setAttribute('type', 'file');
+    fileInput.setAttribute('accept', 'image/*');
+    fileInput.addEventListener('change', () => {
+      if (fileInput.files !== null && fileInput.files[0] !== null) {
+        const range = editor.getSelection(true);
+        const index = range.index + range.length;
+        this.props.onImageUpload(fileInput.files[0])
+          .then(imageUrl => editor.insertEmbed(index, 'image', imageUrl, 'user'));
+      }
+    });
+    fileInput.click();
+  }
+
+  modules = {
+    toolbar: {
+      container: [
+        [{ header: [1, 2, 3, false] }],
+        ['bold', 'italic', 'underline', 'blockquote'],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        ['link', 'image'],
+        ['clean'],
+      ],
+      handlers: {
+        image: this.props.onImageUpload ? this.imageHandler : undefined,
+      },
+    },
+  }
+
+  handleChange = value => this.props.onChange && this.props.onChange(value)
+
+  render() {
+    return (
+      <ReactQuill
+        ref={(el) => { this.quillRef = el; }}
+        value={this.props.value}
+        onChange={this.handleChange}
+        modules={this.modules}
+      />
+    );
+  }
 }
 
-RichEditor.propTypes = {
-  className: PropTypes.string,
-  style: PropTypes.object,
+RichEditor.defaultProps = {
+  value: '',
 };
 
-RichEditor.defaultProps = {
-  className: '',
-  style: {},
+RichEditor.propTypes = {
+  value: PropTypes.any,
+  onChange: PropTypes.func, // eslint-disable-line react/require-default-props
+  onImageUpload: PropTypes.func, // eslint-disable-line react/require-default-props
 };
 
 export default RichEditor;
-
